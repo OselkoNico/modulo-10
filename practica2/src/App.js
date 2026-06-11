5ee157c02ac888a1a207edfcd174b98ec5f07d80
Osel Nicolás <oselko123@gmail.com>
2026-06-11 10:03:00 +0200

    practica 2 - componentes de tipo clase

---
+++ practica2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+          <h1>Bienvenidos a nuestra aplicación</h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
