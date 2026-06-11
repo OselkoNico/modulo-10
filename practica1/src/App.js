752aebc0ef86fa0a2eea15226ba5ba9923d722dc
Osel Nicolás <oselko123@gmail.com>
2026-06-11 09:45:44 +0200

    JSX

--- practica1/src/App.js
+++ practica1/src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let userName = "Pilar";
+
   return (
     <div className="App">
-      <h1>¡Hola Mundo!</h1>
-      <img src={logo} className="App-logo" alt="logo"/>
+      {
+        userName !== null
+        ?
+          <p>Bienvenid@ {userName}</p>
+        :
+          <p>Por favor, inicie sesión</p>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
