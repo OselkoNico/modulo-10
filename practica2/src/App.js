44167f7ff6361a6d8b64db09f3d71044209ef55f
Osel Nicolás <oselko123@gmail.com>
2026-06-15 12:34:27 +0200

    hooks en React

--- practica2/src/App.js
+++ practica2/src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './component/Card';
 import Footer from './component/Footer';
+import { useEffect, useState } from 'react';
 
 function App() {
-
-  const movies = [
-    {id: 1, title: 'Pulp Fiction', director: 'Quentin Tarantino'},
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    //Ejecuta la carga de datos solo una vez
+    setTimeout(() => {
+      setMovies([
+        {id: 1, title: 'Pulp Fiction', director: 'Quentin Tarantino'},
     {id: 2, title: 'Regreso el Futuro', director: 'Robert Zemeckis'},
     {id: 3, title: 'El Padrino', director: 'Francis Ford Coppola'},
-  ]
+      ])
+    }, 2000)
+  }, []) //Array de dependencias vacío
 
   return (
     <>
